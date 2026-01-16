a02a9987d5546873ddeb995cc47a81df329e9ac9
Skanda Vyas <skandavyas20@gmail.com>
2026-01-16 12:07:52 -0800

    Update app icon and add quit on window close

--- Sonexis/SonexisApp.swift
+++ Sonexis/SonexisApp.swift
@@ -1,7 +1,15 @@
 import SwiftUI
 
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+}
+
 @main
 struct SonexisApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
